59ccbec244c2ea83c0cfd8a77a6e778a948d980b
TheMightyChloroplast <isabela.b.78@gmail.com>
2021-11-19 12:27:36 +0000

    added lsystem script

--- Lsystems/Assets/LSystem.cs
+++ Lsystems/Assets/LSystem.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class LSystem : MonoBehaviour
 {
+    private string _axiom;
+    private string _rules;
+    private string _newAxiom;
+    private int _n ;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        _n = 1;
+        _axiom = "F";
+        _rules = "F[+F]F[-F]F";
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        GenerateTree();
+    }
+    
+
+    private void GenerateTree()
+    {
+        for (int i = 0; i <= _n; i++)
+        {
+            foreach (var c in _axiom)
+            {
+                if (c == 'F')
+                {
+                    Debug.Log(_n);
+                    Debug.Log(c);
+                    Debug.Log(_axiom);
+                    
+                    _axiom = _axiom.Replace("F", "F[+F]F[-F]F");
+                    
+                }
+            }
+        }
+
     }
 }
